7869a200ab6a913435d513ef7f2b9eba76f1afa3
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2025-02-03 01:13:41 +0600

    26-01-25

--- resources/views/admin/pages/orderManagement/partial/orderReportTable.blade.php
+++ resources/views/admin/pages/orderManagement/partial/orderReportTable.blade.php
@@ -154,6 +154,10 @@
                                 class="btn btn-sm btn-icon btn-light btn-active-light-primary toggle h-25px w-25px">
                                 <i class="fa-solid fa-eye" title="Order Details"></i>
                             </a>
+                            <a href="{{ route('admin.order.destroy', $order->id) }}"
+                                class="btn btn-sm btn-icon btn-light btn-active-light-primary toggle h-25px w-25px delete">
+                                <i class="fa-solid fa-trash-alt" title="Order Delete"></i>
+                            </a>
                             <button type="button"
                                 class="btn btn-sm btn-icon btn-light btn-active-light-primary toggle h-25px w-25px"
                                 onclick="toggleSubtable(this)">
