c4e471b78b315e48cbe71d1f05f5d20015ceb84d
shahed141123 <khandkershahed23@gmail.com>
2026-01-01 18:47:29 +0600

    Order Management Updated with Server Side loading

--- resources/views/admin/pages/orderManagement/partial/invoice_two.blade.php
+++ resources/views/admin/pages/orderManagement/partial/invoice_two.blade.php
@@ -175,47 +175,4 @@
     </button>
 </div>
 
-@push('scripts')
-    <script>
-        // function downloadInvoice() {
-        //     const invoice = document.querySelector('.card-print');
-        //     html2pdf(invoice, {
-        //         margin: 10,
-        //         filename: `Invoice-${Date.now()}.pdf`,
-        //         image: {
-        //             type: 'jpeg',
-        //             quality: 0.98
-        //         },
-        //         html2canvas: {
-        //             scale: 2
-        //         },
-        //         jsPDF: {
-        //             unit: 'mm',
-        //             format: 'a4',
-        //             orientation: 'portrait'
-        //         }
-        //     });
-        // }
-        window.downloadInvoice = function() {
-            const invoice = document.querySelector('.card-print');
-            if (!invoice) return;
 
-            html2pdf(invoice, {
-                margin: 10,
-                filename: `Invoice-${Date.now()}.pdf`,
-                image: {
-                    type: 'jpeg',
-                    quality: 0.98
-                },
-                html2canvas: {
-                    scale: 2
-                },
-                jsPDF: {
-                    unit: 'mm',
-                    format: 'a4',
-                    orientation: 'portrait'
-                }
-            });
-        };
-    </script>
-@endpush
